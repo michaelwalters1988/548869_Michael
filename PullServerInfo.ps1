28cf3240304dec90cda7ad05683bd42213cc549a
MSW-PULL <MSW-PULL@localhost.local>
2014-09-22 17:16:02 -0500

    MSW-PULL pushing PullServerInfo.ps1

--- PullServerInfo.ps1
+++ PullServerInfo.ps1
@@ -1,6 +1,6 @@
 $pullServerInfo = @{
 "pullServerName" = "MSW-PULL"
-"pullServerPrivateIp" = "10.208.136.118"
-"pullServerPublicIp" = "23.253.111.111"
+"pullServerPrivateIp" = "10.208.3.72"
+"pullServerPublicIp" = "23.253.94.49"
 "region" = "dfw"
 }
